ad0756291c2cff81742e96c1e39d55b8eea6db63
Sergio Rodriguez <SergioR133@icloud.com>
2022-06-02 17:37:59 -0400

    Second Commit

--- 12-http.js
+++ 12-http.js
@@ -6,11 +6,11 @@ const server = http.createServer((req, res) => {
     } else if (req.url === '/about') {
         res.end('Here is our About page') 
     } else {
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `)
+        res.end(`
+            <h1>Oops!</h1>
+            <p>We can't seem to find the page you are looking for</p>
+            <a href="/">back home</a>
+        `)
     }
 })
 
